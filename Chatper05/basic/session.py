5d31a4bef80e19061aef6433be860be999f52e5b
realdevwoong <projung134@gmail.com>
2025-09-04 21:56:23 +0900

    session.py

---
+++ Chatper05/basic/session.py
@@ -0,0 +1,27 @@
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
+from starlette.middleware.sessions import SessionMiddleware
+
+# 환경변수나 랜덤 키 사용 가능
+SECRET_KEY = os.urandom(24)  
+# SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(24))
+
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+
+
+@app.get("/set_session")
+async def set_session(request: Request):
+    request.session["username"] = "woong"
+    return PlainTextResponse("세션 설정 완료")
+
+
+@app.get("/get_session")
+async def get_session(request: Request):
+    username = request.session.get("username")
+    return PlainTextResponse(f"세션 값: {username}")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
